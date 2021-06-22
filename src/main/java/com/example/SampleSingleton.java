cc3a3871ef133d6d7bb14afab11bd7d075609674
Martin Sawilla <martin.sawilla@gmail.com>
2021-06-22 11:20:59 +0200

    Recreated Exception: Multiple possible bean candidates found: [com.example.SampleSingleton, io.micronaut.context.BeanProvider, jakarta.inject.Provider]

---
+++ src/main/java/com/example/SampleSingleton.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+
+@Named("Sample")
+@Singleton
+public class SampleSingleton {
+}
--- src/test/java/com/example/NonUniqueBeanTest.java
+++ src/test/java/com/example/NonUniqueBeanTest.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.inject.qualifiers.Qualifiers;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
@@ -7,15 +9,30 @@ import org.junit.jupiter.api.Assertions;
 
 import jakarta.inject.Inject;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @MicronautTest
 class NonUniqueBeanTest {
 
     @Inject
     EmbeddedApplication<?> application;
 
+    @Inject
+    ApplicationContext applicationContext;
+
     @Test
     void testItWorks() {
-        Assertions.assertTrue(application.isRunning());
+        assertTrue(application.isRunning());
+    }
+
+    @Test
+    void nonUniqueBeanException() {
+        assertEquals(SampleSingleton.class, applicationContext.getBean(SampleSingleton.class).getClass());
+        assertEquals(SampleSingleton.class, applicationContext.getBean(SampleSingleton.class, Qualifiers.byName("Sample")).getClass());
+
+        // This worked with Mn 2.5.x
+        assertEquals(SampleSingleton.class, applicationContext.getBean(Object.class, Qualifiers.byName("Sample")).getClass());
+
     }
 
 }
