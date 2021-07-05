a8aac60f221c7cbd63facee8cebb8c2296b5e01a
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2021-07-05 16:48:28 +0200

    Reproduce bug applicationContext.containsBean

--- src/test/java/com/example/NonUniqueBeanTest.java
+++ src/test/java/com/example/NonUniqueBeanTest.java
@@ -5,7 +5,6 @@ import io.micronaut.inject.qualifiers.Qualifiers;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 
 import jakarta.inject.Inject;
 
@@ -26,12 +25,20 @@ class NonUniqueBeanTest {
     }
 
     @Test
-    void nonUniqueBeanException() {
+    void getBean() {
         assertEquals(SampleSingleton.class, applicationContext.getBean(SampleSingleton.class).getClass());
         assertEquals(SampleSingleton.class, applicationContext.getBean(SampleSingleton.class, Qualifiers.byName("Sample")).getClass());
 
-        // This worked with Mn 2.5.x
+        // This worked with Mn 2.5.x and has been fixed in https://github.com/micronaut-projects/micronaut-core/issues/5639
         assertEquals(SampleSingleton.class, applicationContext.getBean(Object.class, Qualifiers.byName("Sample")).getClass());
+    }
+
+    @Test
+    void containsBean() {
+        assertTrue(applicationContext.containsBean(Object.class, Qualifiers.byName("Sample")));
+
+        // This worked with Mn 2.5.x but is now failing with 3.0.0-M2 and 3.0.0-SNAPSHOT
+        assertFalse(applicationContext.containsBean(Object.class, Qualifiers.byName("some-random-non-existing-name")));
 
     }
 
